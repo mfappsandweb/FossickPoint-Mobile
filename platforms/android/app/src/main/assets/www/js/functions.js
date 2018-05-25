70dfea7179e85c2d55d765a62485ead165b185cb
Unknown <mfappsandweb@gmail.com>
2018-05-25 12:47:46 +0800

    framework: migrated to pure cordova

---
+++ platforms/android/app/src/main/assets/www/js/functions.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+    console.log("Document ready");
+    // Remove deleted card
+    $(".delete-card").click(function(e) {
+        console.log("Remove card clicked");
+        $(this)
+            .parentsUntil(".card-view")
+                .hide();
+    });
+    // Change view
+    $("#page-view").change(function() {
+        console.log("View changed");
+        $("#card-grid").empty();
+        console.log("New View "+$(this).val());
+        switch( $(this).val() ) {
+            case "List View":
+                $("#card-grid").append('<!-- Card List --> \
+                <ul class="list-group col-lg-12 col-sm-12 col-xs-12"> \
+                  <li class="list-group-item"> \
+                      <div class="card-view" id="3"> \
+                          <p>If you can change your mind you can change your life <em>William James</em></p> \
+                          <small class="text-muted"> \
+                              <a href="#" class="delete-card">Delete <i class="fas fa-times"></i></a> \
+                              &nbsp; \
+                              <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                          </small> \
+                      </div> \
+                  </li> \
+                  <li class="list-group-item"> \
+                    <p><a href="">Integrity Builds Trust<img class="list-view-img" style="align: left;" src="img/Integrity-Builds-Trust copy.jpg" alt="Placeholder Image"></a></p> \
+                    <small class="text-muted"> \
+                        <a href="#" class="delete-card">Delete <i class="fas fa-times"></i></a> \
+                        &nbsp; \
+                        <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                    </small> \
+                  </li> \
+                </ul>');
+                break;
+            case "Card View":
+                $("#card-grid").append(' \
+                    <div class="col-lg-4 col-md-6 mb-4 card-view" id="3">\
+                      <div class="card h-100"> \
+                        <div class="quote-container"> \
+                          <p class="quote-text">If you can change your mind you can change your life <br> <em>William James</em></p> \
+                        </div> \
+                        <div class="card-body"> \
+                          <h4 class="card-title"> \
+                            <a href="#">Thought of the day</a> \
+                          </h4> \
+                          <p class="card-text">If you can change your mind you can change your life <br> <em>William James</em></p> \
+                        </div> \
+                        <div class="card-footer"> \
+                          <small class="text-muted"> \
+                            <a href="#" class="delete-card">Delete <i class="fas fa-times"></i></a> \
+                            &nbsp; \
+                            <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                          </small> \
+                        </div> \
+                      </div> \
+                    </div> \
+          \
+                  <div class="col-lg-4 col-md-6 mb-4 card-view" id="1"> \
+                    <div class="card h-100"> \
+                      <img class="card-img-top" src="img/Integrity-Builds-Trust copy.jpg" alt="Placeholder Image"> \
+                      <div class="card-body"> \
+                        <h4 class="card-title"> \
+                          <a href="#">Integrity Builds Trust</a> \
+                        </h4> \
+                      </div> \
+                      <div class="card-footer"> \
+                        <small class="text-muted"> \
+                          <a href="#" onClick("removecard(1);")  class="delete-card">Delete <i class="fas fa-times"></i></a> \
+                          &nbsp; \
+                          <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                        </small> \
+                      </div> \
+                    </div> \
+                  </div> \
+          \
+                  <div class="col-lg-4 col-md-6 mb-4 card-view" id="2"> \
+                    <div class="card h-100"> \
+                      <video class="video-container" autoplay loop> \
+                        <source src="video/The-Launch.mp4" type="video/mp4"> \
+                        This browser doesn\'t support video format \
+                      </video> \
+                      <div class="card-body"> \
+                        <h4 class="card-title"> \
+                          <a href="#">Video Card</a> \
+                        </h4> \
+                        <p class="card-text">This is a demonstration of a video card</p> \
+                      </div> \
+                      <div class="card-footer"> \
+                        <small class="text-muted"> \
+                          <a href="#" onClick("removecard(2);")  class="delete-card">Delete <i class="fas fa-times"></i></a> \
+                          &nbsp; \
+                          <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                        </small> \
+                      </div> \
+                    </div> \
+                  </div> \
+          \
+                  <div class="col-lg-4 col-md-6 mb-4 card-view" id="4"> \
+                      <div class="card h-100"> \
+                        <a href="#"> \
+                          <img class="card-img-top" src="http://placehold.it/700x400" alt="Link Thumbnail Image"> \
+                        </a> \
+                        <div class="card-body"> \
+                          <h4 class="card-title"> \
+                            <a href="#">Link Card</a> \
+                          </h4> \
+                          <p class="card-text">This is a sample link card leading to a toolbox link.</p> \
+                        </div> \
+                        <div class="card-footer"> \
+                          <small class="text-muted"> \
+                            <a href="#" onClick("removecard(4);")  class="delete-card">Delete <i class="fas fa-times"></i></a> \
+                            &nbsp; \
+                            <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                          </small> \
+                        </div> \
+                      </div> \
+                    </div>');
+                break;
+        }
+        //$("#card-view").append("");
+    });
+});
