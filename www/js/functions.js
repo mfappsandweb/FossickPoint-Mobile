ae9a6566bf006bfabad321d34da93c0e49c2010c
mfsoftworks <it@nygmarosebeauty.com>
2018-09-11 17:28:52 +0800

    [js] updated functions.js with new card view print functions

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,14 +1,7 @@
 /**
  * jQuery functions.js
- * @author: Arran Fletcher <it@nygmarosebeauty.com>
+ * @author: Arran Fletcher <mf@nygmarosebeauty.com>
  * @description: Dynamic UI functions and UI features
- * 
- * functions
- *  - removeCardObserver
- *  - myImgObserver
- *  - myVidObserver
- *  - modalObserver
- *  - pageObserver
  * 
  */
 $(document).ready(function() {
@@ -19,27 +12,33 @@ $(document).ready(function() {
 
     // Change view
     $("#page-view").change(function() {
+
         console.log("View changed");
+
         $("#card-grid").empty();
-        console.log("New View "+$(this).val());
+
+        console.log("New View " + $(this).val());
+
         switch( $(this).val() ) {
+
             case "List View":
+
                 $("#card-grid").append('<!-- Card List --> \
                 <ul class="list-group col-lg-12 col-sm-12 col-xs-12"> \
                   <li class="list-group-item" id="3"> \
                     <p class="list-quote-text">If you can change your mind you can change your life<br><em>William James</em></p> \
                     <small class="text-muted"> \
-                        <a href="#" class="delete-card" value="3">Delete <i class="fas fa-times"></i></a> \
+                        <a href="#0" class="delete-card" value="3">Delete <i class="fas fa-times"></i></a> \
                         &nbsp; \
-                        <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                        <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                     </small> \
                   </li> \
                   <li class="list-group-item" id="1"> \
                     <p>Integrity Builds Trust<img class="list-view-img clickableImg" style="align: left;" src="img/Integrity-Builds-Trust copy.jpg" alt="Placeholder Image"></p> \
                     <small class="text-muted"> \
-                        <a href="#" class="delete-card" value="1">Delete <i class="fas fa-times"></i></a> \
+                        <a href="#0" class="delete-card" value="1">Delete <i class="fas fa-times"></i></a> \
                         &nbsp; \
-                        <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                        <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                     </small> \
                   </li> \
                   <li class="list-group-item" id="2"> \
@@ -53,14 +52,15 @@ $(document).ready(function() {
                       </span> \
                     </p> \
                     <small class="text-muted"> \
-                        <a href="#" class="delete-card" value="1">Delete <i class="fas fa-times"></i></a> \
+                        <a href="#0" class="delete-card" value="1">Delete <i class="fas fa-times"></i></a> \
                         &nbsp; \
-                        <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                        <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                     </small> \
                 </ul>');
                 break;
 
             case "Card View":
+
                 $("#card-grid").append(' \
                     <div class="col-lg-4 col-md-6 mb-4 card-view" id="3">\
                       <div class="card h-100"> \
@@ -69,15 +69,15 @@ $(document).ready(function() {
                         </div> \
                         <div class="card-body"> \
                           <h4 class="card-title"> \
-                            <a href="#">Thought of the day</a> \
+                            <a href="#0">Thought of the day</a> \
                           </h4> \
                           <p class="card-text">If you can change your mind you can change your life <br> <em>William James</em></p> \
                         </div> \
                         <div class="card-footer"> \
                           <small class="text-muted"> \
-                            <a href="#" class="delete-card" value="3">Delete <i class="fas fa-times"></i></a> \
+                            <a href="#0" class="delete-card" value="3">Delete <i class="fas fa-times"></i></a> \
                             &nbsp; \
-                            <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                            <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                           </small> \
                         </div> \
                       </div> \
@@ -88,14 +88,14 @@ $(document).ready(function() {
                       <img class="card-img-top clickableImg" src="img/Integrity-Builds-Trust copy.jpg" alt="Placeholder Image"> \
                       <div class="card-body"> \
                         <h4 class="card-title"> \
-                          <a href="#">Integrity Builds Trust</a> \
+                          <a href="#0">Integrity Builds Trust</a> \
                         </h4> \
                       </div> \
                       <div class="card-footer"> \
                         <small class="text-muted"> \
-                          <a href="#" onClick("removecard(1);")  class="delete-card" value="1">Delete <i class="fas fa-times"></i></a> \
+                          <a href="#0" onClick("removecard(1);")  class="delete-card" value="1">Delete <i class="fas fa-times"></i></a> \
                           &nbsp; \
-                          <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                          <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                         </small> \
                       </div> \
                     </div> \
@@ -109,15 +109,15 @@ $(document).ready(function() {
                       </video> \
                       <div class="card-body"> \
                         <h4 class="card-title"> \
-                          <a href="#">Video Card</a> \
+                          <a href="#0">Video Card</a> \
                         </h4> \
                         <p class="card-text">This is a demonstration of a video card</p> \
                       </div> \
                       <div class="card-footer"> \
                         <small class="text-muted"> \
-                          <a href="#" onClick("removecard(2);")  class="delete-card" value="2">Delete <i class="fas fa-times"></i></a> \
+                          <a href="#0" onClick("removecard(2);")  class="delete-card" value="2">Delete <i class="fas fa-times"></i></a> \
                           &nbsp; \
-                          <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                          <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                         </small> \
                       </div> \
                     </div> \
@@ -125,20 +125,20 @@ $(document).ready(function() {
           \
                   <div class="col-lg-4 col-md-6 mb-4 card-view" id="4"> \
                       <div class="card h-100"> \
-                        <a href="#"> \
+                        <a href="#0"> \
                           <img class="card-img-top" src="http://placehold.it/700x400" alt="Link Thumbnail Image"> \
                         </a> \
                         <div class="card-body"> \
                           <h4 class="card-title"> \
-                            <a href="#">Link Card</a> \
+                            <a href="#0">Link Card</a> \
                           </h4> \
                           <p class="card-text">This is a sample link card leading to a toolbox link.</p> \
                         </div> \
                         <div class="card-footer"> \
                           <small class="text-muted"> \
-                            <a href="#" onClick("removecard(4);")  class="delete-card" value="4">Delete <i class="fas fa-times"></i></a> \
+                            <a href="#0" onClick("removecard(4);")  class="delete-card" value="4">Delete <i class="fas fa-times"></i></a> \
                             &nbsp; \
-                            <a href="#" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
+                            <a href="#0" class="favourite-card">Favourite <i class="fas fa-star"></i></a> \
                           </small> \
                         </div> \
                       </div> \
@@ -146,55 +146,192 @@ $(document).ready(function() {
                 break;
 
         }
+
         // Re-add observers
         pageObserver();
     });
 });
 
+// Print quote card
+function printCardQuote(id, quoteText, quoteAuthor, url = "#0") {
+    if(id === null) {
+        throw "Quote Card has empty ID.";
+    }
+
+    var html = '\
+    <div class="col-lg-4 col-md-6 mb-4 card-view" id="' + id + '">\
+        <div class="card h-100"> \
+            <div class="quote-container"> \
+                <p class="card-quote-text"> \
+                    ' + quoteText + ' <br> <em>' + quoteAuthor + '</em> \
+                </p> \
+            </div> \
+            <div class="card-body"> \
+                <h4 class="card-title"> \
+                    <a href="#0">Thought of the day</a> \
+                </h4> \
+                <p class="card-text"> \
+                    ' + quoteText + ' <br> <em>' + quoteAuthor + '</em> \
+                </p> \
+            </div> \
+            <div class="card-footer"> \
+                <small class="text-muted"> \
+                    <a href="#0" class="delete-card" value="' + id + '">Delete <i class="fas fa-times"></i></a> \
+                    &nbsp; \
+                    <a href="#0" class="favourite-card" value="' + id + '">Favourite <i class="fas fa-star"></i></a> \
+                </small> \
+            </div> \
+        </div> \
+    </div>';
+
+    $("#card-grid").prepend(html);
+};
+
+// Print image card
+function printCardImage(id, imageURL, imageAlt = "Fossickpoint Image", clickableURL = "#0", imageCaption) {
+    if(id === null) {
+        throw "Image card has empty ID.";
+    }
+
+    var html = ' \
+    <div class="col-lg-4 col-md-6 mb-4 card-view" id="' + id + '"> \
+        <div class="card h-100"> \
+            <img class="card-img-top clickableImg" src="' + imageURL + '" alt="' + imageAlt + '"> \
+            <div class="card-body"> \
+                <h4 class="card-title"> \
+                    <a href="' + clickableURL + '">' + imageCaption + '</a> \
+                </h4> \
+            </div> \
+            <div class="card-footer"> \
+                <small class="text-muted"> \
+                    <a href="#0" class="delete-card" value="' + id + '">Delete <i class="fas fa-times"></i></a> \
+                    &nbsp; \
+                    <a href="#0" class="favourite-card"  value="' + id + '">Favourite <i class="fas fa-star"></i></a> \
+                </small> \
+            </div> \
+        </div> \
+    </div> \
+    ';
+    
+    $("#card-grid").prepend(html);
+};
+
+// Print video card
+function printCardVideo(id, videoURL, videoType, videoTitle, videoCaption, clickableURL = "#0") {
+    if(id === null) {
+        throw "Video card has empty ID.";
+    }
+
+    var html = ' \
+    <div class="col-lg-4 col-md-6 mb-4 card-view" id="' + id + '"> \
+        <div class="card h-100"> \
+            <video class="video-container" autoplay webkit-playsinline loop poster="img/video.svg"> \
+                <source src="' + videoURL + '" type="' + videoType + '"> \
+                This browser doesn\'t support video format \
+            </video> \
+            <div class="card-body"> \
+                <h4 class="card-title"> \
+                    <a href="' + clickableURL + '">' + videoTitle + '</a> \
+                </h4> \
+                <p class="card-text">' + videoCaption + '</p> \
+            </div> \
+            <div class="card-footer"> \
+                <small class="text-muted"> \
+                    <a href="#0" class="delete-card" value="' + id + '">Delete <i class="fas fa-times"></i></a> \
+                    &nbsp; \
+                    <a href="#0" class="favourite-card" value="' + id + '">Favourite <i class="fas fa-star"></i></a> \
+                </small> \
+            </div> \
+        </div> \
+    </div> \
+    ';
+    
+    $("#card-grid").prepend(html);
+};
+
+// Print weblink preview
+function printLinkPreview(id, imageURL, imageAlt = "Thumbnail Image", clickableURL = "#0", linkTitle, linkCaption) {
+    if(id === null) {
+        throw "Link preview card has empty ID.";
+    }
+
+    var html = ' \
+    <div class="col-lg-4 col-md-6 mb-4 card-view" id="' + id + '"> \
+        <div class="card h-100"> \
+            <a href="' + clickableURL + '"> \
+                <img class="card-img-top" src="' + imageURL + '" alt="' + imageAlt + '"> \
+            </a> \
+            <div class="card-body"> \
+                <h4 class="card-title"> \
+                    <a href="' + clickableURL + '">' + linkTitle + '</a> \
+                </h4> \
+                <p class="card-text">' + linkCaption + '</p> \
+            </div> \
+            <div class="card-footer"> \
+                <small class="text-muted"> \
+                    <a href="#0" class="delete-card" value="' + id + '">Delete <i class="fas fa-times"></i></a> \
+                    &nbsp; \
+                    <a href="#0" class="favourite-card" value="' + id + '">Favourite <i class="fas fa-star"></i></a> \
+                </small> \
+            </div> \
+        </div> \
+    </div> \
+    ';
+    
+    $("#card-grid").prepend(html);
+}
+
 // Run all page observers
 function pageObserver() {
-  removeCardObserver();
-  myImgObserver();
-  myVidObserver();
-  registerObserver();
-  loginObserver();
+
+    removeCardObserver();
+    myImgObserver();
+    myVidObserver();
+    registerObserver();
+    loginObserver();
+
 };
 
 // Check register button
 function registerObserver() {
-  $("#make-user").click(function() {
-    alert("Registering not yet implemented.")
-  });
+    $("#make-user").click(function() {
+        alert("Registering not yet implemented.")
+    });
 };
 
 // Check login form
 function loginObserver() {
-  $("#login-form").submit(function() {
-    alert("Login user: " + $("#username").val() + "\nPassword: " + $("#password").val());
-    loadURL("index.html");
-  });
+    $("#login-form").submit(function() {
+        alert("Login user: " + $("#username").val() + "\nPassword: " + $("#password").val());
+        loadURL("index.html");
+    });
 };
 
 // Remove deleted card
 function removeCardObserver() {
-  $(".delete-card").click(function(e) {
-      console.log("Remove card clicked");
-      var deleteCard = confirm("Delete this item?");
-      if(deleteCard == true) {
-        $('#' + $(this).attr("value") ).hide();
-      }
-  });
+    $(".delete-card").click(function(e) {
+        console.log("Remove card clicked");
+
+        var deleteCard = confirm("Delete this item?");
+
+        if(deleteCard == true) {
+            $('#' + $(this).attr("value") ).hide();
+        }
+    });
 };
 
 // Display image lightbox
 function myImgObserver() {
   $(".clickableImg").click(function() {
+
       console.log("Image tapped. Source " + $(this).attr("src"));
+
       $('nav').before(" \
           <div id='myModal'> \
               <img id='myModalImg' src='"+ $(this).attr("src") +"'> \
           </div>\
       ");
+
       $("#myModal").show();
       modalObserver();
   });
@@ -202,21 +339,25 @@ function myImgObserver() {
 
 // Video lightbox
 function myVidObserver() {
-  $(".clickableVid").click(function() {
-    var src = $(this).find("source").attr("src");
-    var type = $(this).find("source").attr("type");
-    console.log("Clicked video.\nSource: " + src + "\nType: " + type);
-    $('nav').before(" \
-      <div id='myModal'> \
-          <video id='myModalImg' autoplay webkit-playsinline loop> \
-            <source src=" + src + " type=" + type + ">  \
-            This browser doesn't support video format \
-          </video> \
-      </div> \
-    ");
-    $("#myModal").show();
-    modalObserver();
-  });
+    $(".clickableVid").click(function() {
+
+        var src = $(this).find("source").attr("src");
+        var type = $(this).find("source").attr("type");
+
+        console.log("Clicked video.\nSource: " + src + "\nType: " + type);
+
+        $('nav').before(" \
+            <div id='myModal'> \
+                <video id='myModalImg' autoplay webkit-playsinline loop> \
+                    <source src=" + src + " type=" + type + ">  \
+                    This browser doesn't support video format \
+                </video> \
+            </div> \
+        ");
+
+        $("#myModal").show();
+        modalObserver();
+    });
 };
 
 // Close lightbox when no longer needed
@@ -230,5 +371,5 @@ function modalObserver()
 
 // Change Cordova URL
 function loadURL(url) {
-  window.location.assign(url);
+    window.location.assign(url);
 };
